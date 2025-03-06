35bf06b4682b96ac8f458e852c05d109514d0169
devsnowbot <deployment@snowbotix.com>
2025-03-06 14:10:09 -0500

    Update landing_page.sh

--- landing_page.sh
+++ landing_page.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-#Activate the virtual environment
-source $(pwd)/lpenv/bin/activate
-
 #Run the flask web app
-python3 robot-landing-page/app.py > robot-landing-page/app.log 2>&1 &
-
+python3 $(pwd)/app.py > $(pwd)/app.log 2>&1 &
 
 #Run the batterymonitor script
-python3 robot-landing-page/batterymonitor/batteryInfo.py
+python3 $(pwd)/batterymonitor/batteryInfo.py > $(pwd)/batterymonitor/batteryInfo.log 2>&1 &
 
 #print
 echo "Landing page and battery monitor scripts are running."
