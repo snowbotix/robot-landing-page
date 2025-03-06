2b826bd5e9d7caf0bb5bd6a531b9e64709f3cef6
devsnowbot <deployment@snowbotix.com>
2025-03-06 18:13:53 -0500

    Update landing_page.sh

--- landing_page.sh
+++ landing_page.sh
@@ -7,7 +7,7 @@ source $HOME/robot-landing-page/lpenv/bin/activate
 python3 $HOME/app.py > $HOME/app.log 2>&1 &
 
 #Run the batterymonitor script
-python3 $HOME/batterymonitor/batteryInfo.py > $HOME/batterymonitor/batteryInfo.log 2>&1 &
+python3 $HOME/robot-landing-page/batterymonitor/batteryInfo.py 
 
 #print
 echo "Landing page and battery monitor scripts are running."
